b5db62e21ea85407905725ef019f90ff0847c5ab
Bernardozomer <bernardobarzottoz@gmail.com>
2022-02-21 17:59:32 -0300

    Disable tree-sitter indentation for lua

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,20 +1,20 @@
-local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+local status_ok, configs = pcall(require, 'nvim-treesitter.configs')
 if not status_ok then
 	return
 end
 
 configs.setup {
-	ensure_installed = "maintained",
+	ensure_installed = 'maintained',
 	sync_install = false,
-	ignore_install = { "" },
+	ignore_install = { '' },
 	autopairs = {
 		enable = true,
 	},
 	highlight = {
 		enable = true,
-		disable = { "" },
+		disable = { '' },
 		additional_vim_regex_highlighting = true,
 	},
-	indent = { enable = true, disable = { "" } },
+	indent = { enable = true, disable = { '', 'lua' } },
 }
 
