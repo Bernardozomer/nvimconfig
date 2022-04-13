a9ae7f4ce35166e3e1790807dc4aab401940ca87
Bernardozomer <bernardobarzottoz@gmail.com>
2022-04-12 21:28:34 -0300

    Fix conflicting keybinds

--- lua/user/binds.lua
+++ lua/user/binds.lua
@@ -1,5 +1,5 @@
 local opts = { noremap = true, silent = true }
-local term_opts = { silent = true }
+-- local term_opts = { silent = true }
 
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
@@ -25,20 +25,20 @@ keymap('v', '<leader>p', '"+p', opts)
 keymap('n', '<C-s>', ':set hlsearch!<CR>', opts)
 
 -- Better window navigation
-keymap('n', '<C-h>', '<C-w>h', opts)
-keymap('n', '<C-j>', '<C-w>j', opts)
-keymap('n', '<C-k>', '<C-w>k', opts)
-keymap('n', '<C-l>', '<C-w>l', opts)
-
-keymap('i', '<C-h>', '<C-w>h', opts)
-keymap('i', '<C-j>', '<C-w>j', opts)
-keymap('i', '<C-k>', '<C-w>k', opts)
-keymap('i', '<C-l>', '<C-w>l', opts)
-
-keymap('t', '<C-h>', '<C-\\><C-N><C-w>h', term_opts)
-keymap('t', '<C-j>', '<C-\\><C-N><C-w>j', term_opts)
-keymap('t', '<C-k>', '<C-\\><C-N><C-w>k', term_opts)
-keymap('t', '<C-l>', '<C-\\><C-N><C-w>l', term_opts)
+-- keymap('n', '<C-h>', '<C-w>h', opts)
+-- keymap('n', '<C-j>', '<C-w>j', opts)
+-- keymap('n', '<C-k>', '<C-w>k', opts)
+-- keymap('n', '<C-l>', '<C-w>l', opts)
+--
+-- keymap('i', '<C-h>', '<C-w>h', opts)
+-- keymap('i', '<C-j>', '<C-w>j', opts)
+-- keymap('i', '<C-k>', '<C-w>k', opts)
+-- keymap('i', '<C-l>', '<C-w>l', opts)
+--
+-- keymap('t', '<C-h>', '<C-\\><C-N><C-w>h', term_opts)
+-- keymap('t', '<C-j>', '<C-\\><C-N><C-w>j', term_opts)
+-- keymap('t', '<C-k>', '<C-\\><C-N><C-w>k', term_opts)
+-- keymap('t', '<C-l>', '<C-\\><C-N><C-w>l', term_opts)
 
 -- Better window resizing
 keymap('n', '<M-j>', ':resize -1<CR>', opts)
@@ -57,16 +57,16 @@ keymap('t', '<C-_>', '<C-\\><C-n>:call TermToggle(8)<CR>', opts)
 keymap('t', '<esc>', '<C-\\><C-n>', opts)
 
 -- Better buffer navigation
-keymap('n', '<S-l>', ':bnext<CR>', opts)
-keymap('n', '<S-h>', ':bprevious<CR>', opts)
+keymap('n', '<C-l>', ':bnext<CR>', opts)
+keymap('n', '<C-h>', ':bprevious<CR>', opts)
 
 -- Replace all
 keymap('n', 'S', ':%s///g<Left><Left><Left>', opts)
 
 -- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
-keymap("v", "p", '"_dP', opts)
+keymap('v', '<A-j>', ':m .+1<CR>==', opts)
+keymap('v', '<A-k>', ':m .-2<CR>==', opts)
+keymap('v', 'p', '"_dP', opts)
 
 -- Plugin: telescope
 keymap('n', '<leader>f', '<cmd>lua require(\'telescope.builtin\').find_files()<CR>', opts)
